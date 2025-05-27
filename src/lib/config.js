8060facbef226f0fdc56302c22eb472e0a8205cc
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-27 13:11:38 +0300

    Create config.js

---
+++ src/lib/config.js
@@ -0,0 +1,5 @@
+export const APPWRITE_ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+export const APPWRITE_PROJECT = import.meta.env.VITE_APPWRITE_PROJECT;
+export const APPWRITE_DB = import.meta.env.VITE_APPWRITE_DB;
+export const COLLECTION_ID = import.meta.env.VITE_APPWRITE_APARTMENT_COLLECTION_ID;
+export const BUCKET_ID = import.meta.env.VITE_APPWRITE_BUCKET_ID;
