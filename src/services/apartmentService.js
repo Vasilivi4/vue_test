1d5a14b8266eaf21e5dc9893be01fe601dd3019a
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-27 13:11:59 +0300

    Create apartmentService.js

---
+++ src/services/apartmentService.js
@@ -0,0 +1,5 @@
+import { databases } from "@/lib/appwrite";
+import { APPWRITE_DB, COLLECTION_ID } from "@/lib/config.js";
+import { Query } from "appwrite";
+
+// TODO: Write functions to create, update, delete and get apartment/apartments
