eb2efa3496f50860e4f82887962a1840e7033c62
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-29 01:34:10 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
+import { registerSW } from 'virtual:pwa-register'
 
 import "./style.css";
 
 createApp(App).use(router).mount("#app");
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('Доступна нова версія. Оновити?')) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log('Програма готова для офлайн-режиму.')
+  },
+})
