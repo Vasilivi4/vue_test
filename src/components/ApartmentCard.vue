4c6b865c5d5a5ee4d50adfdd7f6af66c61f17006
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-28 07:10:24 +0300

    Update ApartmentCard.vue

--- src/components/ApartmentCard.vue
+++ src/components/ApartmentCard.vue
@@ -1,7 +1,12 @@
 <script setup>
-defineProps(["apartment"]);
+defineProps(['apartment'])
 </script>
 
 <template>
-  <div class="border p-4 rounded shadow">...</div>
+  <div class="border p-4 rounded shadow p-4">
+    <h2 class="text-xl font-semibold">{{ apartment.name }}</h2>
+    <p>Ціна: {{ apartment.price }} грн</p>
+    <p>Кімнат: {{ apartment.rooms }}</p>
+    <p>Площа: {{ apartment.square }} м²</p>
+  </div>
 </template>
