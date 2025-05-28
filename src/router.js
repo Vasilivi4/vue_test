57b9877fd297ac0fe51637f44bde0094934cd6d3
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-28 07:10:46 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -3,12 +3,16 @@ import Home from "./pages/Home.vue";
 import Auth from "./pages/Auth.vue";
 import ListingDetails from "./pages/ListingDetails.vue";
 import CreateEditListing from "./pages/CreateEditListing.vue";
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: Home },
     { path: "/auth", name: "Auth", component: Auth },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/register', name: 'Register', component: Register },
     { path: "/listing/:id", name: "ListingDetails", component: ListingDetails },
     { path: "/edit/:id?", name: "CreateEditListing", component: CreateEditListing },
   ],
